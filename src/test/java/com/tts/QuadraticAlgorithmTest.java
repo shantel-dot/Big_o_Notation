cf0621d1694004d9e22e2c197be756f127f71899
shantel-dot <taylorteep2@yahoo.com>
2021-01-25 19:01:45 -0600

    Initial commit

---
+++ src/test/java/com/tts/QuadraticAlgorithmTest.java
@@ -0,0 +1,40 @@
+package com.tts;
+
+import java.util.Collection;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+@RunWith(Parameterized.class)
+public class QuadraticAlgorithmTest extends TestBase {
+
+    @Parameterized.Parameters
+    public static Collection input() {
+        return TEST_SIZE_PARAMETER;
+    }
+
+    private int[] integerArray;
+
+    private QuadraticAlgorithm testClass;
+
+    public QuadraticAlgorithmTest(int nSize) {
+        super();
+        this.nSize = nSize;
+    }
+
+    @Test
+    public void insertion_sort() {
+        testClass.insertion_sort(integerArray);
+    }
+
+    @Before
+    public void setup() {
+        testClass = new QuadraticAlgorithm();
+        integerArray = setArray(this.nSize);
+        this.filename = FileNameConstants.POLY_TIME;
+        super.setup();
+    }
+
+}
